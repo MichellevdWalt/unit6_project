faa7f729fef84cd6f7380318d2d91549a4528d85
Michelle <62473111+MichellevdWalt@users.noreply.github.com>
2020-05-03 13:59:21 +0200

    Final commit

---
+++ app.js
@@ -0,0 +1,56 @@
+//require express
+const express = require("express");
+
+//require data form data.json file
+const {projects} = require("./data.json");
+
+const app = express();
+
+//Use pug as view engine
+app.set('view engine', 'pug');
+
+//Use all static files found in the public folder
+app.use('/static', express.static('public'));
+
+//Route home page
+app.get('/', (req, res) => {
+    res.render('index', {projects});
+})
+
+//Route about page
+app.get('/about', (req, res) => {
+    res.render('about', {projects});
+})
+
+//route project page according to project selection and send data to pug file to render. 
+//If statement checks for valid route for one of the projects, if not next(error is returned).
+app.get('/project/:id', (req, res, next) =>{
+   if (req.params.id < 5) {
+       res.render('project',{
+        project: projects[req.params.id]
+       }) }else {
+           const error = new Error(`Oops, that page doesn't exist`);
+           error.status = 404;
+           console.log (`Error: ${error}`);
+           return next(error);
+       }    
+})
+
+//All other pages that are not routed, creates custom error.
+app.use((req, res, next) =>{
+    const error = new Error(`Oops, this page doesn't exist`);
+    error.status = 404;
+    console.log(`Something went wrong: ${error}`);
+    return next(error);
+});
+
+//Create error message and render error.pug
+app.use((error, req, res, next) => {
+    res.locals.error = error;
+    res.status(error.status);
+    res.render(`error`);
+});
+
+app.listen(3000, () => {
+     console.log('The application is running on localhost:3000');
+ })
